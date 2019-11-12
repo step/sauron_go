4c784bf86dfeeb9b16ebbecdaee1c210053be526
Chandan Kumar <cksharma122@gmail.com>
2019-11-12 17:46:00 +0530

    Basic version of sauron
    - Implement server
    - Marshaled payload
    - Enqueued queue with angmar message

---
+++ pkg/main/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/step/saurontypes"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type Repo struct {
+	Name string `json:name`
+}
+
+type Pusher struct {
+	Name string `json:"name"`
+}
+
+type Payload struct {
+	Repository Repo
+	Pusher     Pusher
+}
+
+func getMessage(repoName string, pusher string) saurontypes.AngmarMessage {
+	url := "https://api.github.com/repos/__NAME__/__REPO__/tarball/refs/heads/master"
+	url = strings.Replace(url, "__NAME__", pusher, 1)
+	url = strings.Replace(url, "__REPO__", repoName, 1)
+	msg := saurontypes.AngmarMessage{
+		Url:     url,
+		SHA:     "master",
+		Pusher:  pusher,
+		Project: repoName,
+		Tasks: []saurontypes.Task{
+			{Queue: "test", ImageName: "orc_sample"},
+			{Queue: "lint", ImageName: "orc_sample"},
+		},
+	}
+	return msg
+}
+
+func githubPushListner(res http.ResponseWriter, r *http.Request) {
+	flag.Parse()
+	defer r.Body.Close()
+	b, _ := ioutil.ReadAll(r.Body)
+	var payload Payload
+	json.Unmarshal(b, &payload)
+	repoName := payload.Repository.Name
+	pusher := payload.Pusher.Name
+
+	redisClient := GetRedisClient()
+	message, _ := json.Marshal(getMessage(repoName, pusher))
+
+	redisClient.Enqueue(queueName, string(message))
+	fmt.Println(string(message))
+
+	res.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	http.HandleFunc("/", githubPushListner)
+	http.ListenAndServe(":3333", nil)
+}
